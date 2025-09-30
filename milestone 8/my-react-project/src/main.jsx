00787d2f3f0733edd49361eb8a6eeae803a1c025
siham binta habib <sihambintahabib@gmail.com>
2025-09-30 14:26:26 +0600

    milestone 8

--- milestone 8/my-react-project/src/main.jsx
+++ milestone 8/my-react-project/src/main.jsx
@@ -4,17 +4,34 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Header from './components/header/Header.jsx';
+import Home from './components/home/Home.jsx';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>HELLO FROM REACT ROUTER</div>,
+    // element: <div>Element : HELLO FROM REACT ROUTER</div>,
+  },
+  {
+    path: "app",
+    element: <div>Element : HELLO FROM app</div>
+    // Component: App
+  },
+  {
+    path: "header",
+    element: <div>Element:  HELLO FROM Header</div>
+    // Component: Header
+  },
+  {
+    path: "home",
+    // element: <div>Element : HELLO FROM home</div>
+    Component: Home 
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />,
+    <RouterProvider router={router} />
   </StrictMode>,
 )
